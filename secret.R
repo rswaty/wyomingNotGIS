42ad6739b13fbb209d5e4600d018f7eba02c3d39
rswaty <rswaty@tnc.org>
2020-12-16 08:20:37 -0500

    added PPT

--- secret.R
+++ secret.R
@@ -1,22 +1,27 @@
 
 
-# get current data formatted for joining to reference condition data
+# get CURRENT data formatted for joining to reference condition data
 
 library(dplyr)
 library(readr)
 
+#remove decimals
+options(digits = 0)
+#prevent scientific notation in outputs
+options(scipen = 999)
+
 # read in combine data that has all of the current data from the ArcGIS combine of LANDFIRE Existing Vegetation Cover/Height/Type plus succession classes (a.k.a. seral states) and Biophysical Settings (historical ecosystems)
+ 
+combine200s <- read_csv("rawData/combine200s.csv")  # this is essentially the "current data"
 
-combine200s <- read_csv("rawData/combine200s.csv")
-View(combine200s)
+#View(combine200s)
 
+# need to get rid of unwanted columns and isolate the succession classes for each BpS
 BpSscl <- combine200s %>%
   group_by(BPS_MODEL, LABEL) %>%
   summarize(count = sum(COUNT))
 
 ## need percents of s-classes per BpS
-
-options(scipen = 999)       
 
 current <- group_by(BpSscl, BPS_MODEL) %>% 
               mutate(percent = (count/sum(count))*100)
